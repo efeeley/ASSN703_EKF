5980e9321ae8f3a3108de6f890482fbddb97ff6d
efeeley <elisabeth.k.feeley@gmail.com>
2017-07-05 13:11:29 -0400

    Add project files.

---
+++ ASSN703_EKF/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASSN703_EKF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare Variables
+            int num;
+            string numstring;
+
+            //Loop
+            do
+            {
+                //User Input
+                Console.WriteLine("Enter a negative number:");
+                numstring = Console.ReadLine();
+                num = Convert.ToInt32(numstring);
+            }
+            while (num >= 0);
+        }
+    }
+}
